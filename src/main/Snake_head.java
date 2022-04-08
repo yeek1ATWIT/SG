7f3da93df3f6671edf70d416ad27908004a66312
yeek1ATWIT <yeek1@wit.edu>
2022-04-08 10:38:26 -0400

    Add files via upload

---
+++ src/main/Snake_head.java
@@ -0,0 +1,48 @@
+package main;
+import java.awt.Color;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Rectangle2D;
+import java.util.ArrayList;
+
+import plugin.FRCustom.*;
+
+public class Snake_head extends Snake_part {
+    
+    private static final Color color = new Color(100,200,100);
+    
+    public Snake_head(int x, int y) {
+        this.x = x;
+        this.y = y;
+        this.Snake_part_GUI.setShape(new Rectangle2D.Double(this.x, this.y, d.getWidth(), d.getHeight()));
+        this.Snake_part_GUI.setColor(color);
+        Snake_part.Hierarchy.add(this.Snake_part_GUI);
+        appendHierarchy();
+    }
+    
+    private void appendHierarchy() {
+        //Snake_part.Snake_GUI = new FRCanvas(Hierarchy.toArray());
+        Snake_part.Snake_GUI.setOpaque(false);
+        sg.Game_GUI1.setOpaque(false);
+        sg.Game_GUI2.setOpaque(false);
+        
+    }
+
+    public void setX(int x) {
+        this.x = x;
+        this.Snake_part_GUI.setShape(new Rectangle2D.Double(this.x, this.y, d.getWidth(), d.getHeight()));
+        
+    }
+
+    public void setY(int y) {
+        this.y = y;
+        this.Snake_part_GUI.setShape(new Rectangle2D.Double(this.x, this.y, d.getWidth(), d.getHeight()));
+        
+    }
+
+    public void setDirection(int direction) {
+        this.direction = direction;
+        this.Snake_part_GUI.setShape(new Rectangle2D.Double(this.x, this.y, d.getWidth(), d.getHeight()));
+        //this.Snake_part_GUI.setRotationZ(direction);
+        
+    }
+}
